46275ea8be7cbc4ea0a69dce67fa01df973fdbce
Robin Mattern <robin.mattern@gmail.com>
2024-10-16 10:03:02 -0400

    c41016.03_Update run-anyllm.sh

--- run-anyllm.sh
+++ run-anyllm.sh
@@ -9,15 +9,26 @@
 # ---------------------------------------------------------------------------
 
         aArg1=$1; aArg2=$2; aArg3=$3; aCmd=""
+  if [ "${aArg1:0:5}" == "setup" ]; then aCmd="setup";  fi
+
   if [ "${aArg1:0:3}" == "cop" ] && [ "${aArg2:0:3}" == "env" ]; then aCmd="copyEnvs";  fi
+
   if [ "${aArg1:0:3}" == "sta" ] && [ "${aArg2:0:3}" == "app" ]; then aCmd="startApp";  fi
+  if [ "${aArg1:0:3}" == "sta" ] && [ "${aArg2:0:1}" == "c"   ]; then aCmd="startApp";  aArg3="c"; fi
+  if [ "${aArg1:0:3}" == "sta" ] && [ "${aArg2:0:1}" == "f"   ]; then aCmd="startApp";  aArg3="f"; fi
+  if [ "${aArg1:0:3}" == "sta" ] && [ "${aArg2:0:1}" == "s"   ]; then aCmd="startApp";  aArg3="s"; fi
+
   if [ "${aArg1:0:3}" == "sto" ] && [ "${aArg2:0:3}" == "app" ]; then aCmd="stopApp";   fi
+  if [ "${aArg1:0:3}" == "sto" ] && [ "${aArg2:0:1}" == "c"   ]; then aCmd="stopApp";  aArg3="c"; fi
+  if [ "${aArg1:0:3}" == "sto" ] && [ "${aArg2:0:1}" == "f"   ]; then aCmd="stopApp";  aArg3="f"; fi
+  if [ "${aArg1:0:3}" == "sto" ] && [ "${aArg2:0:1}" == "s"   ]; then aCmd="stopApp";  aArg3="s"; fi
 
   # ---------------------------------------------------------------------------
 
   if [ "${aCmd}" == "" ]; then
      echo ""
      echo "  AnyLLM Commands"
+     echo "    Setup           Run yarn setup for AnythingLLM"
      echo "    Copy envs       Copy .env.example files to .env files"
      echo "    Start {App}     Start AnyLLM App: collector, frontend or server"
      echo "    Stop {App}      Stop AnyLLM App: collector, frontend or server"
@@ -26,8 +37,13 @@
      fi
 # ---------------------------------------------------------------------------
 
+  if [ "${aCmd}" == "setup" ]; then
+     cd "${aRepos}${aProj}${aStage}"
+     yarn setup
+     fi
+# ---------------------------------------------------------------------------
+
   if [ "${aCmd}" == "copyEnvs" ]; then
-     aStage="dev03-robin"; 
      cp -p "${aRepos}/${aProj}${aStage}/collector/.env.example"          "${aRepos}/${aProj}${aStage}/collector/.env"
      cp -p "${aRepos}/${aProj}${aStage}/frontend/.env.example"           "${aRepos}/${aProj}${aStage}/frontend/.env"
      cp -p "${aRepos}/${aProj}${aStage}/server/.env.development.example" "${aRepos}/${aProj}${aStage}/server/.env.development"
